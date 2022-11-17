6af03b94b5043275125dbd89815d125cd1da9dad
aabhisekh <116128257+aabhisekh@users.noreply.github.com>
2022-11-17 14:22:05 -0500

    Update abhi.py

--- abhi.py
+++ abhi.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import glob
+import os
+import time
+import pandas as pd
+from PIL import ImageFile, Image
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 st.markdown("# Abhi's Pics")
 st.sidebar.markdown("# Abhi")
+path = "/app/abhi/abhi/Pics"
+filenames = glob.glob(os.path.join(path, "*"))
+placeholder = st.empty()
+
+for filename in filenames:
+    image = Image.open(filename)
+    placeholder.image(image, caption='Abhi latest Pics')
+    time.sleep(4)
