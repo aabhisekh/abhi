e1f173b5563950a9e04f747efa63856b99c8a2f9
aabhisekh <116128257+aabhisekh@users.noreply.github.com>
2023-10-01 12:56:59 -0400

    Update Turkey.py

--- Turkey.py
+++ Turkey.py
@@ -8,7 +8,7 @@ from PIL import ImageFile, Image
 
 #st.markdown("# Turkey & Egypt Few Pics - Mom and Abhi")
 st.sidebar.markdown("# Mom and Abhi")
-path = "/apps/abhi/Turkey"
+path = "/apps/aabhisekh/abhi/Turkey"
 filenames = glob.glob(os.path.join(path, "*"))
 placeholder = st.empty()
 
